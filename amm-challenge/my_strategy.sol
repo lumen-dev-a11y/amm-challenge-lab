4eae7bd93693b415d06adabb66d8db1e1975ab0b
lumen-dev-a11y <michaelliuml666@gmail.com>
2026-03-06 11:10:06 +0800

    night-opt best 523.79 (night_7)

--- amm-challenge/my_strategy.sol
+++ amm-challenge/my_strategy.sol
@@ -26,10 +26,10 @@ contract Strategy is AMMStrategyBase {
     uint256 constant MIN_GATE = 30000000000000000; // 0.03 WAD
 
     // --- Cubic Toxicity ---
-    uint256 constant TOX_CUBIC_COEF = 14222 * BPS;
+    uint256 constant TOX_CUBIC_COEF = 13662 * BPS;
 
     // --- Trade-Tox Boost ---
-    uint256 constant TRADE_TOX_BOOST = 2854 * BPS;
+    uint256 constant TRADE_TOX_BOOST = 2530 * BPS;
 
     // --- Asymmetric Stale Dir ---
     uint256 constant STALE_ATTRACT_FRAC = 1124000000000000000; // 1.124
@@ -45,13 +45,13 @@ contract Strategy is AMMStrategyBase {
     uint256 constant BASE_FEE = 3 * BPS;
     uint256 constant SIGMA_COEF = 200000000000000000; // 0.20
     uint256 constant LAMBDA_COEF = 12 * BPS;
-    uint256 constant FLOW_SIZE_COEF = 4600 * BPS;
-    uint256 constant TOX_COEF = 382 * BPS;
-    uint256 constant TOX_QUAD_COEF = 12616 * BPS;
-    uint256 constant ACT_COEF = 99079 * BPS;
+    uint256 constant FLOW_SIZE_COEF = 4779 * BPS;
+    uint256 constant TOX_COEF = 315 * BPS;
+    uint256 constant TOX_QUAD_COEF = 12228 * BPS;
+    uint256 constant ACT_COEF = 94855 * BPS;
     uint256 constant DIR_COEF = 20 * BPS;
     uint256 constant DIR_TOX_COEF = 100 * BPS;
-    uint256 constant STALE_DIR_COEF = 6456 * BPS;
+    uint256 constant STALE_DIR_COEF = 6666 * BPS;
     uint256 constant SIGMA_TOX_COEF = 500 * BPS;
     uint256 constant TAIL_KNEE = 500 * BPS;
     uint256 constant TAIL_SLOPE_PROTECT = 930000000000000000; // 0.93
@@ -285,6 +285,6 @@ contract Strategy is AMMStrategyBase {
     }
 
     function getName() external pure override returns (string memory) {
-        return "night_3";
+        return "night_7";
     }
 }
